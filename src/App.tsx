b931e0ddded24d9cc55a95c51e529abbf998de58
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-08-28 16:11:50 +0300

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import GlobalStyle from "./ts/globalStyle";
 import Container from "./components/Container";
 import Results from "./components/Results";
 import Search from "./components/Search";
-import { useQuery, QueryClient } from "@tanstack/react-query";
-import { getJokes, getApiJokes } from "./ts/api";
+import { useQuery } from "@tanstack/react-query";
+import { getApiJokes } from "./ts/api";
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
 import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
     const searchTimeOut = 1000;
     const searchQuery = useSelector((state: RootState) => state.app.value);
     const [filter, setFilter] = useState("");
-    const { isLoading, isError, data, error, isInitialLoading } = useQuery({
+    const { isError, data, error, isInitialLoading } = useQuery({
         queryKey: ["jokes", filter],
         queryFn: () => getApiJokes(filter),
         enabled: filter.length > 3,
     });
 
     useEffect(() => {
-        const delaySearch = setTimeout(
-            () => setFilter(searchQuery),
-            searchTimeOut
-        );
+        const delaySearch = setTimeout(() => {
+            if (searchQuery.length > 3) setFilter(searchQuery);
+        }, searchTimeOut);
         return () => clearTimeout(delaySearch);
     }, [searchQuery]);
 
@@ -32,10 +32,18 @@ function App() {
             <Container>
                 <Search />
 
-                {/* {isLoading && <span>Loading...</span>} */}
-                {/* {isError && error instanceof Error && (
+                {isInitialLoading && (
+                    <span style={{ marginTop: "10rem" }}>
+                        <ClipLoader
+                            size={150}
+                            aria-label="Loading Spinner"
+                            data-testid="loader"
+                        />
+                    </span>
+                )}
+                {isError && error instanceof Error && (
                     <span>Error: {error.message}</span>
-                )} */}
+                )}
                 {data && <Results data={data.data.result} />}
             </Container>
         </>
